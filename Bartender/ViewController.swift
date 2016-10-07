40e2cd4ad1681a30930e1004057bec39467cf96b
Damián Tapia Nava <damian@MacBook-Pro-de-Damian.local>
2016-10-07 12:23:01 -0500

    Login

---
+++ Bartender/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bartender
+//
+//  Created by Damián Tapia Nava on 07/10/16.
+//  Copyright © 2016 Damián Tapia Nava. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
